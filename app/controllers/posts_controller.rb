005a73e3a42801808d6d17d88c76489dacf8afd8
Shardendu <shardendukumargautam@gmail.com>
2015-11-11 23:48:21 +0530

    updating post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
 
+	# List all posts
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
+	# Form for adding a post
 	def new
 		@post = Post.new
 	end
 
+	# Add a post
 	def create
 		@post = Post.new(post_params)
 		if @post.save
@@ -17,8 +20,25 @@ class PostsController < ApplicationController
 		end
 	end
 
+	# Show a post
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	# Form for editing a post
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	# Update a post
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	private
